f4b0a4bb15c9166f804e2fa52ead7f06b28fb7bc
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-09 21:23:00 +0100

    First pass building method.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
-def fizzbuzz(number)
-  'fizz'
+def fizzbuzz(num)
+  # Only need to check if a number when creating the method outside of the Integer class
+  return 'You need to input a number' unless num.is_a? Integer
+
+  return 'fizzbuzz' if num % 15 == 0
+  return 'fizz' if num % 3 == 0
+  return 'buzz' if num % 5 == 0
+  return num
 end
