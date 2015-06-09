6125657a6d8c9fa399726e1e70ada4d733d5564e
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-09 22:17:19 +0100

    Refactoring using a hash instead of if statements.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,6 @@
 class Integer < Numeric
   def fizzbuzz
-    # # Only need to check if a number when creating the method outside of the Integer class
-    # return 'You need to input a number' unless num.is_a? Integer
-
-    return 'fizzbuzz' if self % 15 == 0
-    return 'fizz' if self % 3 == 0
-    return 'buzz' if self % 5 == 0
-    return self
+    rules = {'fizz'=> self % 3 ==0 && self % 5 !=0, 'buzz'=> self % 5 == 0 && self % 3 !=0, 'fizzbuzz'=> self % 3 == 0 && self % 5 == 0 }
+    rules.key(true) || self
   end
 end
