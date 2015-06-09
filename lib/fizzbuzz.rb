f4dadf3fc3ee0977d2e74d285de0a2f77031d893
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-09 22:20:10 +0100

    Making hash easier to read.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 class Integer < Numeric
   def fizzbuzz
-    rules = {'fizz'=> self % 3 ==0 && self % 5 !=0, 'buzz'=> self % 5 == 0 && self % 3 !=0, 'fizzbuzz'=> self % 3 == 0 && self % 5 == 0 }
+    rules = {'fizz'=> self % 3 ==0 && self % 5 !=0,
+             'buzz'=> self % 5 == 0 && self % 3 !=0,
+             'fizzbuzz'=> self % 3 == 0 && self % 5 == 0 }
     rules.key(true) || self
   end
 end
