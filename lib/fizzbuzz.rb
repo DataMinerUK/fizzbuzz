2c30c88dfcc469c91d59096d56c389a0be0f67d0
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-09 21:27:18 +0100

    Extending Integer class to include method fizzbuzz.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,11 @@
-def fizzbuzz(num)
-  # Only need to check if a number when creating the method outside of the Integer class
-  return 'You need to input a number' unless num.is_a? Integer
+class Integer < Numeric
+  def fizzbuzz
+    # # Only need to check if a number when creating the method outside of the Integer class
+    # return 'You need to input a number' unless num.is_a? Integer
 
-  return 'fizzbuzz' if num % 15 == 0
-  return 'fizz' if num % 3 == 0
-  return 'buzz' if num % 5 == 0
-  return num
+    return 'fizzbuzz' if self % 15 == 0
+    return 'fizz' if self % 3 == 0
+    return 'buzz' if self % 5 == 0
+    return self
+  end
 end
